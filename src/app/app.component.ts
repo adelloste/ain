2b078fc2cbe1894b5c9ac202b3ca33b02a64dd35
Alessandro <adelloste@minsait.com>
2020-08-28 11:36:14 +0200

    tracking app - gtag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit }     from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+
+import { filter } from 'rxjs/operators';
+
+declare let gtag: Function;
 
 @Component({
   selector: 'ain-root',
@@ -7,8 +12,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.stats();
+  }
+
+  stats(): void {
+    this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd),
+    )
+    .subscribe(
+      (event: NavigationEnd) => {
+        gtag('config', 'UA-96184893-1', { 'page_path': event.urlAfterRedirects });
+      }
+    );
+  }
 
 }
